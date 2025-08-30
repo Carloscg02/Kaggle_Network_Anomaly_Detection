cd3f39641cd14bfdb78c8922790b4913d3a59db8
Carloscg02 <carloscg.002@gmail.com>
2025-08-30 12:48:12 +0200

    Update dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -1,7 +1,7 @@
 # src/dataset.py
 import pandas as pd
-#from src.config import DATA_FILE  #comment to work in notebook
-from config import DATA_LABELED,DATA_UNLABELED #comment to work in model.py
+#from src.config import DATA_FILE  #comment to work in model.py
+from config import DATA_LABELED,DATA_UNLABELED #comment to work in notebook
 
 # def load_data():
 #     return pd.read_csv(DATA_FILE)
